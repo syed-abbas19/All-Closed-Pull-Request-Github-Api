2018ac703cc4a78ca809b7f3736b796785a20063
Abhay Tomar <tomarabhay767@gmail.com>
2023-02-18 00:21:15 +0530

    Updated files

---
+++ README.md
@@ -0,0 +1,37 @@
+# All Closed Pull Request from Github Api
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+Steps to use:
+
+1- Setup Flutter 
+
+2- Clone the repo
+
+3-Install dependencies
+
+  - $ flutter pub get
+  
+4- Run App
+  
+  - $ flutter run
+  
+# Screenshot
+
+![image](https://user-images.githubusercontent.com/75438874/218331650-dd2a7016-a47a-40ae-ba81-a7af0f7f80a4.png)
+
+
+
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/views/home_page.dart
@@ -0,0 +1,153 @@
+// ignore_for_file: prefer_interpolation_to_compose_strings
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
+import '../model/model.dart';
+
+import 'package:shimmer/shimmer.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<PostModel> postList = [];
+
+  Future<List<PostModel>> getPostApi() async {
+    final resposne = await http.get(Uri.parse(
+        'https://api.github.com/repos/abhay-tomar03/-Pull-Request-GithubApi-App/pulls?state=closed'));
+    
+    var data = jsonDecode(resposne.body.toString());
+    if (resposne.statusCode == 200) {
+      postList.clear();
+      for (Map<String, dynamic> i in data) {
+        postList.add(PostModel.fromJson(i));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('All Closed Pull Request'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+              future: getPostApi(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                   return SizedBox(
+                          height: 200,
+                          width: double.infinity,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(16),
+                            child: Image.network(
+                              'https://picsum.photos/250?image=9',
+                              fit: BoxFit.cover,
+                              loadingBuilder: (context, child, loadingProgress) =>
+                                  Shimmer.fromColors(
+                                baseColor: Colors.grey[300]!,
+                                highlightColor: Colors.white,
+                                child: Container(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ),
+                          )
+                    );
+                } else {
+                  return ListView.builder(
+                      itemCount: postList.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Image.network(
+                                      postList[index].user.avatarUrl,
+                                      height: 60,
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                Column(
+                                  children: [
+                                    Text(
+                                      'Title: ' +
+                                          postList[index].title.toString(),
+                                      style: const TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Column(
+                                  children: [
+                                    Text(" Created at : " +
+                                        postList[index].createdAt.toString()),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                Column(
+                                  children: [
+                                    Text(" Closed at : " +
+                                        postList[index].closedAt.toString()),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Column(
+                                  children: [
+                                    Text(
+                                        'Username : ' +
+                                            postList[index]
+                                                .user
+                                                .login
+                                                .name
+                                                .toLowerCase(),
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyLarge),
+                                  ],
+                                ),
+                                const Divider()
+                              ],
+                            ),
+                          ),
+                        );
+                      });
+                }
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
